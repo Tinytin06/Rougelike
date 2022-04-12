104c1796d1020f4fd5acf4c1f66ad54582e72ea9
Tinytin06 <52684983+Tinytin06@users.noreply.github.com>
2022-04-12 13:11:21 -0700

    testing if objects work
    testing if objects work

---
+++ Assets/Scripts/Hi.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Hi {
+    public int number;
+    Hi() {
+        int number = 0;
+    }
+}
---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Hi;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Hi hi = new Hi();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
